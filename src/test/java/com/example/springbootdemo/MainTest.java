e537b410b27cdeb5187648018b7084c91c42c88b
song <xudsong@163.com>
2019-01-10 12:41:47 +0800

    增加手机号、身份证等信息脱敏工具类

--- src/test/java/com/example/springbootdemo/MainTest.java
+++ src/test/java/com/example/springbootdemo/MainTest.java
@@ -1,13 +1,17 @@
 package com.example.springbootdemo;
 
+import com.example.springbootdemo.util.CommonUtils;
 import com.xiaoleilu.hutool.util.IdcardUtil;
 
 public class MainTest {
 
     public static void main(String[] args){
         String idCard = "362321199011015934";
+        String mobile = "18720061484";
 
         //身份证验证
+        System.out.println(CommonUtils.idEncrypt(idCard));
+        System.out.println(CommonUtils.mobileEncrypt(mobile));
         System.out.println(IdcardUtil.isValidCard(idCard));
     }
 }
